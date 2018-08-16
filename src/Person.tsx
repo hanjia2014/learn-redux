91e52a61ffbb2a4cf753d22d76d834ebf4093862
Han Jia <admin@admins-iMac-2.local>
2018-08-17 11:02:11 +1200

    upgrade typescript

--- src/Person.tsx
+++ src/Person.tsx
@@ -6,22 +6,23 @@ import axios from 'axios'
 export class PersonDiv extends React.Component<any, { users: Array<User> }> {
     private Users: User[];
     private userView: Array<JSX.Element>;
+    private showSpinner: boolean = false;
 
     constructor(props: any) {
         super(props);
         this.state = { users: []};
-        this.getData();
     }
 
     componentDidMount() {
-        this.getData();
+        //this.getData();
     }
 
     render() {
         return <div>
+                <input type="button" onClick={this.getData} value="Get Users"></input>
                 <ClipLoader
                     color={'#123abc'}
-                    loading={this.state.users.length == undefined || this.state.users.length == 0}
+                    loading={this.showSpinner && (this.state.users.length == undefined || this.state.users.length == 0)}
                 />
                 {this.state.users.map((user, index) => {
                     return <div className="col-md-12" style={{ color: index % 2 == 0 ? 'red' : 'green', backgroundColor: index % 2 == 0 ? 'lightgrey' : 'yellow' }} key={user.id}>{user.email}</div>
@@ -30,10 +31,12 @@ export class PersonDiv extends React.Component<any, { users: Array<User> }> {
     }
 
     getData = () => {
+        this.showSpinner = true;
         axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
         axios.get('https://jsonplaceholder.typicode.com/users')
             .then((response) => {
-                this.setState({ users: response.data });                
+                this.setState({ users: response.data });    
+                this.showSpinner = false;            
             });
     }
 }
