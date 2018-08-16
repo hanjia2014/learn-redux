404dc06324d2fb86ffff40fcd2e4d5b04c0e63f7
Han Jia <admin@admins-iMac-2.local>
2018-08-17 11:37:46 +1200

    code updated

--- src/Person.tsx
+++ src/Person.tsx
@@ -3,14 +3,13 @@ import { User } from "./models/User";
 import { ClipLoader } from 'react-spinners';
 import axios from 'axios'
 
-export class PersonDiv extends React.Component<any, { users: Array<User> }> {
+export class PersonDiv extends React.Component<any, { users: Array<User>, showSpinner: boolean }> {
     private Users: User[];
     private userView: Array<JSX.Element>;
-    private showSpinner: boolean = false;
 
     constructor(props: any) {
         super(props);
-        this.state = { users: []};
+        this.state = { users: [], showSpinner: false};
     }
 
     componentDidMount() {
@@ -20,23 +19,28 @@ export class PersonDiv extends React.Component<any, { users: Array<User> }> {
     render() {
         return <div>
                 <input type="button" onClick={this.getData} value="Get Users"></input>
-                <ClipLoader
-                    color={'#123abc'}
-                    loading={this.showSpinner && (this.state.users.length == undefined || this.state.users.length == 0)}
-                />
-                {this.state.users.map((user, index) => {
-                    return <div className="col-md-12" style={{ color: index % 2 == 0 ? 'red' : 'green', backgroundColor: index % 2 == 0 ? 'lightgrey' : 'yellow' }} key={user.id}>{user.email}</div>
-                })}
+                <div className="row">
+                    {this.state.showSpinner &&
+                        <ClipLoader
+                            color={'#123abc'}
+                            loading={this.state.users.length == undefined || this.state.users.length == 0}
+                        />
+                    }
+                
+                    {this.state.users.map((user, index) => {
+                        return <div className="col-md-12" style={{ color: index % 2 == 0 ? 'red' : 'green', backgroundColor: index % 2 == 0 ? 'lightgrey' : 'yellow' }} key={user.id}>{user.email}</div>
+                    })}
+                </div>
             </div>;
     }
 
     getData = () => {
-        this.showSpinner = true;
+        this.setState({ showSpinner: true });
         axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
         axios.get('https://jsonplaceholder.typicode.com/users')
             .then((response) => {
                 this.setState({ users: response.data });    
-                this.showSpinner = false;            
+                this.setState({ showSpinner: false });            
             });
     }
 }
